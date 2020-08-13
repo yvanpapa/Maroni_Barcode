9c65f43ac96d82c1173825e4b21ec812d44e5d3c
yvanpapa <49006752+yvanpapa@users.noreply.github.com>
2020-08-13 19:52:54 +1200

    Update Figure4_script.R

--- Figure4_script.R
+++ Figure4_script.R
@@ -2,6 +2,8 @@
 #Inputs needed: 
 #Figure 1 environment
 #Session 2 environment
+###Maroni shapefile env is not enough anymore (raster update). Now you also need:
+#shapefiles directory with sa_dem_30s.bil
 
 library(rasterVis)
 library(rgdal)
@@ -9,21 +11,16 @@ library(rgdal)
 library(grid)
 #grid.text
 
-session_Fig1<-"your_session_directory_here"
-session_Fig1<-"E:/BARCODE_MARONI/SCRIPTS_AND_DATA/FIGURE1"
-
 session2<-"your_session2_directory_here"
-session2<-"E:/BARCODE_MARONI/SCRIPTS_AND_DATA/LANDSCAPES_PIPELINE/2_Landcapes"
 load(paste0(session2,"/env_landscapes_all_data.RData")) #~10 sec
 
+session_Fig1<-"your_Fig1_directory_here"
+
 session_Fig4<-"your_session_directory_here"
-session_Fig4<-"E:/BARCODE_MARONI/SCRIPTS_AND_DATA/FIGURE4"
 setwd(session_Fig4)
 
-#IDEA FOR AN EVEN LIGHTER VERSION: USE SA_RIVS 15S INSTEAD OF 30S?
-
 ####
-
+basemap<-raster("shapefiles/sa_dem_30s.bil")
 crop(basemap,ext_basemap)->basemap2  
 readOGR(paste0(session_Fig1,"/shapefiles"),"maroni_rivs")->maroni_rivs2
 levelplot(basemap2,margin=F,colorkey=F,region=F)->rast1 #rast1= croppped south america
@@ -74,34 +71,37 @@ dev.off()
 
 save.image("Figure4_data.RData")
 
+######## LIGHT VERSION ##########################
+
+dev.off()
+dev.new()
+#plot.new()
+levelplot(rnorm_xyz_total.masked2,col.regions=cols_gr,at=seq(0, 1, length.out=1001), colorkey = list(space='right'),
+          margin=F,main=list(label="Mean \"multispecies\" genetic landscape",cex=1.4),scales=list(draw=T,cex=1.2))->rast2  #colorkey(title=label_norm,title.gpar=list(fontsize=20))
+rast2+layer(sp.points(SpatialPoints(coord_tot[,1:2]),pch=21,col='black',fill="white",cex=1))->r2l2
+r2l2+as.layer(rast1+lay1,under=T)->plot
+print(plot)
+dev.print(pdf, paste0('Figure4A_gr_light.pdf')) # takes ~30sec to open with Adobe
+dev.print(svg, paste0('Figure4A_gr_light.svg')) # takes ~1 min to open with inkscape
+dev.print(png, paste0('Figure4A_gr_light.png'),width = 1024, height = 768)
+dev.off()
+
+dev.off()
+dev.new()
+levelplot(count_total.masked2,col.regions=cols_tr,at=seq(0, 100, length.out=11), colorkey = list(space='right',labels=list(cex=1.8)),
+          margin=F,main=list(label="Species landscapes coverage"),scales=list(draw=T,cex=1.5))->rast2  #colorkey(title=label_norm,title.gpar=list(fontsize=20))
+#rast2+layer(sp.points(SpatialPoints(coord_tot[,1:2]),pch=21,col='black',fill="white",cex=1))->r2l2
+print(rast2)
+#grid.text(x=.08,y=.91,label="A",gp=gpar(fontsize=22, col="BLACK",fontface="bold"))
+#grid.text(x=.55,y=.91,label="B",gp=gpar(fontsize=22, col="BLACK",fontface="bold"))
+dev.print(pdf, paste0('Figure4B_light.pdf')) # takes ~30sec to open with Adobe
+dev.print(svg, paste0('Figure4B_light.svg')) # takes ~1 min to open with inkscape
+dev.print(png, paste0('Figure4B_light.png'),width = 1024, height = 768)
+dev.off()
+
+save.image("Figure4_data.RData")
+
 
 #The final manuscript version has been modified on Inkscape
-
-####COLORED VERSION PRODUCES A VERY HEAVY PDF. NOT OPENABLE WITH ADOBE OR INKSCAPE####
-
-#crop(basemap,ext_basemap)->basemap2  
-#myTheme <- BTCTheme()
-#myTheme$panel.background$col = 'dodgerblue3' #for the sea
-#readOGR(paste0(session_Fig1,"/shapefiles"),"maroni_rivs")->maroni_rivs2
-#levelplot(basemap2,col.regions=(pal(1000)),margin=F,colorkey=FALSE)->rast1 #rast1= croppped south america
-#layer(sp.polygons(maroni_basins))+layer(sp.polygons(maroni_rivs2, col = "dodgerblue3", lwd=1))->lay1   #basins limits and rivers
-#extend(rnorm_xyz_total.masked,ext_basemap)->rnorm_xyz_total.masked2
-#extend(count_total.masked,ext_basemap)->count_total.masked2
-
-#dev.off()
-#dev.new()
-##plot.new()
-#levelplot(rnorm_xyz_total.masked2,par.settings = myTheme,col.regions=cols_tr,at=seq(0, 1, length.out=11), colorkey = list(space='right'),margin=F,main=list(label="All species",cex=1.5),scales=list(draw=T,cex=1.5))->rast2  #colorkey(title=label_norm,title.gpar=list(fontsize=20))
-#rast2+layer(sp.points(SpatialPoints(coord_tot[,1:2]),pch=21,col='black',fill="white",cex=1))->r2l2
-#r2l2+as.layer(rast1+lay1,under=T)->plot
-#print(plot,split=c(1,1,2,1),newpage=F)
-
-#levelplot(count_total.masked2,par.settings = myTheme,col.regions=cols_tr,at=seq(0, 100, length.out=11), colorkey = list(space='right'),margin=F,main=list(label="Number of species computed by pixel",cex=1.5),scales=list(draw=T,cex=1.5))->rast2  #colorkey(title=label_norm,title.gpar=list(fontsize=20))
-#rast2+layer(sp.points(SpatialPoints(coord_tot[,1:2]),pch=21,col='black',fill="white",cex=1))->r2l2
-#r2l2+as.layer(rast1+lay1,under=T)->plot
-#print(plot,split=c(2,1,2,1),newpage=F)
-#dev.print(pdf, paste0('Figure4.pdf'))
-#dev.print(png, paste0('Figure4.png'),width = 1024, height = 768)
-#dev.off()
 
 
